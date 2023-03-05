f187f5bd28206738c4abb40b298262d23c1c2c0a
siddhardha123 <siddhardha123123@gmail.com>
2023-03-06 00:16:33 +0530

    Initial Commit

---
+++ sampleapp/sampleappApp.swift
@@ -0,0 +1,17 @@
+//
+//  sampleappApp.swift
+//  sampleapp
+//
+//  Created by Sid Varma on 06/03/23.
+//
+
+import SwiftUI
+
+@main
+struct sampleappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
